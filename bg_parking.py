bad6fea784af9f66683cdf5cd28570ba26761523
Jacob Felknor <jacobfelknor073@gmail.com>
2021-03-07 08:06:50 -0700

    mv keys, rename test.py

--- bg_parking.py
+++ bg_parking.py
@@ -3,18 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from keys import password, username
+from keys import chromedriver, password, url, username
 
 option = webdriver.ChromeOptions()
 option.add_argument("-incognito")
 option.add_argument("--headless")
 # option.add_argument("disable-gpu")
 
-browser = webdriver.Chrome(
-    executable_path=r"/home/jacob/bin/chromedriver", options=option
-)
+browser = webdriver.Chrome(executable_path=chromedriver, options=option)
 
-browser.get("https://www.bgvgrandcentral.com/")
+browser.get(url)
 
 user = WebDriverWait(browser, 20).until(
     EC.presence_of_element_located(
