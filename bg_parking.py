fb8995b3f99af57033f9716f4d9968957e1a8987
Jacob Felknor <jacobfelknor073@gmail.com>
2022-01-29 10:15:51 -0700

    added headless back in

--- bg_parking.py
+++ bg_parking.py
@@ -40,7 +40,7 @@ def email_confirmation(msg: str = None, msg_prefix: str = "") -> None:
 
 option = webdriver.ChromeOptions()
 option.add_argument("-incognito")
-# option.add_argument("--headless")
+option.add_argument("--headless")
 # option.add_argument("disable-gpu")
 
 
